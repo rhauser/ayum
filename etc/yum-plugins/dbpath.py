5b2be90ba532f3cc3a446534c88aa8c84811e3d3
Reiner Hauser <Reiner.Hauser@cern.ch>
2013-10-14 15:03:09 +0200

    change plugin configuration

--- etc/yum-plugins/dbpath.py
+++ etc/yum-plugins/dbpath.py
@@ -1,14 +1,25 @@
+"""
+YUM plugin to configure RPM dbpath and prefix value.
+The dbpath option should be in the [main] section, while
+the prefix option can be used both in the [main] and the [repo]
+sections.
+"""
+
+from yum import config
 from yum.plugins import PluginYumExit, TYPE_CORE, TYPE_INTERACTIVE
 
 requires_api_version = '2.3'
 plugin_type = (TYPE_CORE, TYPE_INTERACTIVE)
 
-def init_hook(conduit):
-    print "Init"
-
 def config_hook(conduit):
-    #help(conduit)
-    dbpath = conduit.confString('main','dbpath','')
+    config.YumConf.dbpath = config.Option('')
+    config.YumConf.prefix = config.Option('')
+    config.RepoConf.prefix = config.Inherit(config.YumConf.prefix)
+
+def init_hook(conduit):
+    conf = conduit.getConf();
+    dbpath = conf.dbpath
     if dbpath != '':
       from rpm import addMacro
       addMacro('_dbpath',dbpath)
+    
